81f21658e300783668f1e33ad6b284d3299b903b
meganatc7 <meganatc7@gmail.com>
2021-05-22 23:26:13 +0900

    Add Movie page

---
+++ vuejs/src/components/MovieList.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <nav class="d-flex mt-5 align-items-center">
+      <div class="me-5">
+        <span class="fs-3 fw-bold">기타 영화</span>
+      </div>
+      <div class="filter">
+        <span :class="{ pick: state == 1 }" @click=all>전체</span>
+        <span :class="{ pick: state == 2 }" @click=action>액션</span>
+        <span :class="{ pick: state == 3 }" @click=comedy>코미디</span>
+        <span :class="{ pick: state == 4 }" @click=horror>공포</span>
+        <span :class="{ pick: state == 5 }" @click=animation>애니메이션</span>
+        <span :class="{ pick: state == 6 }" @click=sf>SF</span>
+      </div>
+    </nav>
+    <div class="row row-cols-4">
+      <MovieListItem
+        v-for="movie in movieList"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieListItem from '@/components/MovieListItem'
+
+export default {
+  name: 'MovieList',
+  components: {
+    MovieListItem,
+  },
+  data() {
+    return {
+      state: 1,
+    }
+  },
+  computed: {
+    movieList() {
+      return this.$store.getters.getMovieList
+    }
+  },
+  methods: {
+    all() {
+      this.state = 1
+    },
+    action() {
+      this.state = 2
+    },
+    comedy() {
+      this.state = 3
+    },
+    horror() {
+      this.state = 4
+    },
+    animation() {
+      this.state = 5
+    },
+    sf() {
+      this.state = 6
+    },
+  }
+}
+</script>
+
+<style>
+  nav span {
+    color: white;
+  }
+  .filter span {
+    margin-right: 15px;
+    cursor: pointer;
+  }
+
+  .filter span:hover {
+    text-shadow: 0 0 5px yellow;
+    font-size: 17px;
+  }
+
+  .pick {
+    color: yellow;
+    font-size: 17px;
+  }
+</style>
--- vuejs/src/store/index.js
+++ vuejs/src/store/index.js
@@ -9,19 +9,31 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     userInfo: {},
+    username: '',
     token: localStorage.getItem('token') || '',
+    movieList: [],
   },
   getters: {
     isAuthenticated(state) {
       return state.token ? true : false
-    }
+    },
+    getUserInfo(state) {
+      return state.username
+    },
+    getMovieList(state) {
+      return state.movieList
+    },
   },
   mutations: {
     CREATE_USER(state, userInfo) {
       state.userInfo = userInfo
     },
-    AUTH_USER(state, token) {
-      state.token = token
+    AUTH_USER(state, payload) {
+      state.token = payload.token
+      state.username = payload.username
+    },
+    GET_MOVIE_LIST(state, movieList) {
+      state.movieList = movieList
     }
   },
   actions: {
@@ -35,12 +47,17 @@ export default new Vuex.Store({
     async AUTH_USER({ commit }, userInfo) {
       const AUTH_USER_URL = '/api/token/'
       const data = userInfo
-      console.log('hi')
       const response = await axios.post(AUTH_USER_URL, data)
       console.log(response)
       const token = response.data.access
+      const payload = {'username': userInfo.username, 'token': token}
       localStorage.setItem('token', token)
-      commit('AUTH_USER', token)
+      commit('AUTH_USER', payload)
+    },
+    async GET_MOVIE_LIST({ commit }) {
+      const MOVIE_LIST_URL = '/api/v1/movies/'
+      const response = await axios.get(MOVIE_LIST_URL)
+      commit('GET_MOVIE_LIST', response.data)
     }
   },
   modules: {
