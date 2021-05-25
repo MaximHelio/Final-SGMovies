cd226b70bb9024ba9e9a31a960ab4179f08b491d
meganatc7 <meganatc7@gmail.com>
2021-05-26 00:24:17 +0900

    Modify comment feature

--- final_pjt_back/movies/models.py
+++ final_pjt_back/movies/models.py
@@ -23,6 +23,7 @@ class Movie(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=100)
     rank = models.IntegerField(default=0)
